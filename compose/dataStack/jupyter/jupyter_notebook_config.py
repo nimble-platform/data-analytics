64e2e3d0c4b71861e6588caefbe91ce677b027ab
Christoph Schranz <christoph.schranz@salzburgresearch.at>
2018-05-14 16:50:36 +0200

    Make it runnable

--- compose/dataStack/jupyter/jupyter_notebook_config.py
+++ compose/dataStack/jupyter/jupyter_notebook_config.py
@@ -11,30 +11,20 @@ c = get_config()
 c.NotebookApp.ip = '*'
 c.NotebookApp.port = 8888
 c.NotebookApp.open_browser = False
-c.NotebookApp.password = '' # 'sha1:bcd259ccf...<your hashed password here>'
+#c.NotebookApp.password = '' # 'sha1:bcd259ccf...<your hashed password here>'
 c.NotebookApp.token = '30bbf31e2c867cd06f7ed80306e2abcb9768dba6'  # enables login without any security layers
-#c.NotebookApp.allow_password_change=False
 
-# Generate a self-signed certificate
-if 'GEN_CERT' in os.environ:
-    dir_name = jupyter_data_dir()
-    pem_file = os.path.join(dir_name, 'notebook.pem')
-    try:
-        os.makedirs(dir_name)
-    except OSError as exc:  # Python >2.5
-        if exc.errno == errno.EEXIST and os.path.isdir(dir_name):
-            pass
-        else:
-            raise
-    # Generate a certificate if one doesn't exist on disk
-    subprocess.check_call(['openssl', 'req', '-new',
-                           '-newkey', 'rsa:2048',
-                           '-days', '365',
-                           '-nodes', '-x509',
-                           '-subj', '/C=XX/ST=XX/L=XX/O=generated/CN=generated',
-                           '-keyout', pem_file,
-                           '-out', pem_file])
-    # Restrict access to the file
-    os.chmod(pem_file, stat.S_IRUSR | stat.S_IWUSR)
-    c.NotebookApp.certfile = pem_file
+
+# run behind an apache2 proxy, tutorial:
+# https://linode.com/docs/applications/big-data/install-a-jupyter-notebook-server-on-a-linode-behind-an-apache-reverse-proxy/https://linode.com/docs/applications/big-data/install-a-jupyter-notebook-server-on-a-linode-behind-an-apache-reverse-proxy/
+
+c.NotebookApp.allow_origin = 'http://127.0.0.1:8888/jupyter'
+c.NotebookApp.base_url = '/jupyter'  # OK
+#c.NotebookApp.certfile = '/absolute/path/to/mycert.pem'
+#c.NotebookApp.ip = '�localhost' # geht ned, il060 a ned
+#c.NotebookApp.keyfile = '/absolute/path/to/mykey.key'
+#c.NotebookApp.open_browser = False
+#c.NotebookApp.password = 'sha1:30bbf31e2c867cd06f7ed80306e2abcb9768dba6'  # enables login without any security layers, doesn't work
+c.NotebookApp.trust_xheaders = True # doesn't work
+
 
